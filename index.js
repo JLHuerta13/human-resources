346002d4fc100cfa0e274618525fcba093a8d736
Jorge Huerta <jorgeluishuerta3@gmail.com>
2022-12-11 16:06:26 -0600

    act 5

--- index.js
+++ index.js
@@ -1,11 +1,20 @@
 const express = require('express');
 const app = express();
 
+/* Ruta "/" */ 
 app.get("/", (req, res, next) => {
     res.status(200);
-    res.send("Bienvenido");
+    res.send("Bienvenido ");
 });
 
-app.listen(3000, () => {
+/* Ruta "/name" */
+app.get("/:name", (req, res, next) => {
+    console.log(req.params.name);
+    res.status(200);
+    res.send("Estas en la pagina nombre " + req.params.name); 
+})
+
+/*Inicializacion de servidor*/ 
+app.listen(process.env.PORT || 3000, () => {
     console.log("Server is running...");
 });
