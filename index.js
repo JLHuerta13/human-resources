8463a8db2de4f03e6e366975ddb78de8ffc654cb
Jorge Huerta <jorgeluishuerta3@gmail.com>
2022-12-11 21:39:43 -0600

    act 5

--- index.js
+++ index.js
@@ -1,18 +1,24 @@
 const express = require('express');
 const app = express();
+/* Enlace con la base de datos */
+const {employee} = require('./employees.json');
 
 /* Ruta "/" */ 
 app.get("/", (req, res, next) => {
     res.status(200);
-    res.send("Bienvenido ");
+    res.send("Bienvenido");
 });
 
 /* Ruta "/name" */
-app.get("/:name", (req, res, next) => {
-    console.log(req.params.name);
+app.get('/employee', (req, res, next) => {
+    res.status(200); 
+    res.send(employee);
+});
+
+app.get('/employee/:id', (req, res, next) => {
     res.status(200);
-    res.send("Estas en la pagina nombre " + req.params.name); 
-})
+    res.send(employee[req.params.id - 1]);
+});
 
 /*Inicializacion de servidor*/ 
 app.listen(process.env.PORT || 3000, () => {
